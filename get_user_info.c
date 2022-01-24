2deb976fb34ebbdced5fe6d31f276fff9e9173f1
Mmmr7 <ol-sidorenkov@mail.ru>
2022-01-24 15:18:32 +0300

    some more includes and comments

--- get_user_info.c
+++ get_user_info.c
@@ -1,7 +1,9 @@
 // Input / Output library.
 #include <stdio.h>
-// Calling OS functions library.
+// Calling OS functions libraries.
 #include <unistd.h>
+// Used only for uid_t type, currently this include is pointless.
+#include <sys/types.h>
 
 int main(int argc, char** argv) {
 	printf("User ID is %d and their Group ID is %d!\n", getuid(), getgid());
